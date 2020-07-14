14457e464f05389826b4bc08dffad4985580da9c
Jacob Tan <jacobtyq@gmail.com>
2020-07-15 01:23:23 +0800

    Remove unused tests

--- src/util/utils.js
+++ src/util/utils.js
@@ -1,32 +1,51 @@
-const fs = require('fs')
+const fs = require("fs");
 
-const writeToFile = (filename, data) => {
+const writeToFile = async (filename, data) => {
   return fs.writeFile(filename, data, (error) => {
-    if (error) throw error
-    console.log('The file has been saved!')
-  })
-}
+    if (error) throw error;
+    console.log(`The file ${filename} has been saved!`);
+  });
+};
 
 const camelCaseToDash = (string) => {
-  return string.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase()
-}
+  return string.replace(/([a-z])([A-Z])/g, "$1-$2").toLowerCase();
+};
 
 const flattenArray = (arr, d = 1) => {
-  return d > 0 ? arr.reduce((acc, val) => acc.concat(Array.isArray(val) ? flattenArray(val, d - 1) : val), [])
-    : arr.slice()
-}
+  return d > 0
+    ? arr.reduce(
+        (acc, val) =>
+          acc.concat(Array.isArray(val) ? flattenArray(val, d - 1) : val),
+        []
+      )
+    : arr.slice();
+};
 
 const findAllByValue = (obj, valueToFind) => {
-  return Object.entries(obj)
-    .reduce((acc, [key, value]) => (value === valueToFind)
-      ? acc.concat(({ id: Object.values(obj.id).join(''), name: Object.values(obj.name).join('') }))
-      : (typeof value === 'object')
+  return Object.entries(obj).reduce(
+    (acc, [key, value]) =>
+      value === valueToFind
+        ? acc.concat({
+            id: Object.values(obj.id).join(""),
+            name: Object.values(obj.name).join(""),
+          })
+        : typeof value === "object"
         ? acc.concat(findAllByValue(value, valueToFind))
-        : acc
-    , [])
-}
+        : acc,
+    []
+  );
+};
 
-exports.writeToFile = writeToFile
-exports.camelCaseToDash = camelCaseToDash
-exports.flattenArray = flattenArray
-exports.findAllByValue = findAllByValue
+const createFolder = async (path) => {
+  try {
+    await fs.promises.access(path, fs.constants.F_OK);
+  } catch (err) {
+    await fs.promises.mkdir(path);
+  }
+};
+
+exports.writeToFile = writeToFile;
+exports.camelCaseToDash = camelCaseToDash;
+exports.flattenArray = flattenArray;
+exports.findAllByValue = findAllByValue;
+exports.createFolder = createFolder;
