926c5f262dac10d0604445b4f9b11b36f63afab7
Jacob Tan <jacobtyq@gmail.com>
2020-07-15 01:23:48 +0800

    First cut: Pull SVGs from a Node

--- src/index.js
+++ src/index.js
@@ -1,36 +1,80 @@
-require('dotenv').config()
-const axios = require('axios')
-const figmaRestApi = require('./util/figmaRestApi')
-const Utils = require('./util/utils')
+require("dotenv").config();
+const { performance } = require("perf_hooks");
+const axios = require("axios");
+const figmaRestApi = require("./util/figmaRestApi");
+const Utils = require("./util/utils");
+const outputFolder = "./src/svg/";
+const rateLimit = 20;
 
 const svgHelper = async () => {
   try {
-    console.log('url', 'files/' + process.env.FIGMA_BASE_URL + process.env.FIGMA_PROJECT_ID + '/nodes?ids=' + process.env.FIGMA_PROJECT_NODE_ID)
-    const response = await figmaRestApi.get('files/' + process.env.FIGMA_PROJECT_ID + '/nodes?ids=' + process.env.FIGMA_PROJECT_NODE_ID)
-    const children = await response.data.nodes[process.env.FIGMA_PROJECT_NODE_ID].document.children
+    console.log(performance.now());
+    console.log(
+      "url",
+      "files/" +
+        process.env.FIGMA_BASE_URL +
+        process.env.FIGMA_PROJECT_ID +
+        "/nodes?ids=" +
+        process.env.FIGMA_PROJECT_NODE_ID
+    );
+    const response = await figmaRestApi.get(
+      "files/" +
+        process.env.FIGMA_PROJECT_ID +
+        "/nodes?ids=" +
+        process.env.FIGMA_PROJECT_NODE_ID
+    );
+    const children = await response.data.nodes[
+      process.env.FIGMA_PROJECT_NODE_ID
+    ].document.children;
 
-    console.log('Number of SVGs', Utils.findAllByValue(children, 'INSTANCE').length)
-    const svgs = Utils.findAllByValue(children, 'INSTANCE')
+    const svgs = Utils.findAllByValue(children, "INSTANCE");
+    const numOfSvgs = svgs.length;
 
-    const data = await Promise.all(
-      svgs.map(async (svg, index) => {
+    console.log("Number of SVGs", numOfSvgs);
+
+    Utils.createFolder(outputFolder);
+
+    for (i = 0; i < numOfSvgs; i += rateLimit) {
+      const requests = svgs.slice(i, i + rateLimit).map(async (svg) => {
         // Get URL of each SVG
-        let svgName = await svg.name
+        let svgName = await svg.name;
 
-        if (svgName.includes('/')) {
-          const nameArr = svg.name.split('/').join('-')
-          svgName = nameArr
+        if (svgName.includes("/")) {
+          const nameArr = svg.name.split("/").join("-");
+          svgName = nameArr;
         }
-        const svgURL = await figmaRestApi.get('images/' + process.env.FIGMA_PROJECT_ID + '/?ids=' + svg.id + '&format=svg')
-        // Get SVG DOM
-        const svgDOM = await axios.get(svgURL.data.images[svg.id])
-        Utils.writeToFile('./src/svg/' + `${Utils.camelCaseToDash(svgName)}.svg`, svgDOM.data)
-        // return svgDOM.data
-      })
-    )
-  } catch (err) {
-    console.error(err)
-  }
-}
 
-svgHelper()
+        const svgURL = await figmaRestApi.get(
+          "images/" +
+            process.env.FIGMA_PROJECT_ID +
+            "/?ids=" +
+            svg.id +
+            "&format=svg"
+        );
+
+        // Get SVG DOM
+        const svgDOM = await axios.get(svgURL.data.images[svg.id]);
+        Utils.writeToFile(
+          outputFolder + `${Utils.camelCaseToDash(svgName)}.svg`,
+          svgDOM.data
+        );
+      });
+
+      await Promise.all(requests)
+        .then(() => {
+          console.log("Wait for 45 seconds");
+          return new Promise(function (resolve) {
+            setTimeout(() => {
+              console.log("45 seconds!");
+              resolve();
+            }, 45000);
+          });
+        })
+        .catch((err) => console.error(`Error proccessing ${i} - Error ${err}`));
+    }
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+svgHelper();
